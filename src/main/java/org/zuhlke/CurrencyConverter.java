ddc399ef339302ce51bc6747987e9f7d034b6be2
Besser, Florian <Florian.Besser@zuehlke.com>
2018-08-21 21:10:25 +0800

    Add converter

---
+++ src/main/java/org/zuhlke/CurrencyConverter.java
@@ -0,0 +1,10 @@
+package org.zuhlke;
+
+import java.math.BigDecimal;
+
+public class CurrencyConverter {
+
+    public BigDecimal getConversionRate(String from, String to) {
+        throw new RuntimeException("I'm an awkward collaborator, you should not call me!");
+    }
+}
