0ed28d0e18d08039d2cb227c7cec04a5f165842d
Besser, Florian <Florian.Besser@zuehlke.com>
2018-08-22 11:28:41 +0800

    Updated Pojo

--- src/main/java/org/zuhlke/basket/dao/BasketPojo.java
+++ src/main/java/org/zuhlke/basket/dao/BasketPojo.java
@@ -1,20 +1,27 @@
 package org.zuhlke.basket.dao;
 
-import org.zuhlke.item.Item;
-
 import javax.persistence.*;
-import java.util.HashMap;
 import java.util.Map;
 
 @Entity
 public class BasketPojo {
-
     @Id
-    public int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
 
     @ElementCollection
     @MapKeyColumn(name = "item")
     @Column(name = "amount")
     @CollectionTable(name = "basket_attributes", joinColumns = @JoinColumn(name = "basket_id"))
-    Map<Item, Integer> attributes = new HashMap<>(); // maps from attribute name to value
+    public Map<String, Integer> itemMap; // maps from attribute name to value
+
+
+    public BasketPojo() {
+        // Default constructor for JPA
+    }
+
+    public BasketPojo(Integer id, Map<String, Integer> itemMap) {
+        this.id = id;
+        this.itemMap = itemMap;
+    }
 }
