75d5c8b5bb63d90ddd4e30f22682dd129bb82b77
Besser, Florian <Florian.Besser@zuehlke.com>
2018-08-22 16:04:42 +0800

    Added first REST endpoint

---
+++ src/main/java/org/zuhlke/basket/BasketRest.java
@@ -0,0 +1,17 @@
+package org.zuhlke.basket;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
+
+@Path("/")
+public class BasketRest {
+
+    @GET
+    @Produces("application/json")
+    public List<Basket> getBaskets() {
+        // TODO
+        return null;
+    }
+}
